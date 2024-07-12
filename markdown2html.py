5629a3368bdf95c8f56f8100d465ed8a1965e2cc
talk2dennis <adigwedennis@yahoo.com>
2024-07-12 09:40:39 +0100

    completed checks if file exists: added name check

--- markdown2html.py
+++ markdown2html.py
@@ -4,11 +4,11 @@
 import sys
 import os
 
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
-
-if os.path.exists(sys.argv[1]) is False:
-    print("Missing {}".format(sys.argv[1]), file=sys.stderr)
-    sys.exit(1)
+    if os.path.exists(sys.argv[1]) is False:
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
